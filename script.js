23e509a0da62b3361878dbccc8b84dce5fd07c60
HitarthPatel123 <hitarthpadaliya@gmail.com>
2024-08-11 18:23:10 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,43 @@
+const searchBox=document.querySelector('#location');
+const searchBtn=document.querySelector('#searchBtn');
+const weather_img=document.getElementById('weather-img');
+const temp=document.querySelector('.temp');
+const nature=document.querySelector('.nature');
+const perc=document.querySelector('.perc');
+const speed=document.querySelector('.speed');
+
+async function fetchWeather(city){
+    const apikey="01c855835bac8bc316b944aadad7ab77";
+    const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
+
+    const weather_data=await fetch(`${url}`).then(response=>
+        response.json());
+    
+    // console.log(weather_data);
+    temp.innerHTML=`${Math.round(weather_data.main.temp-273.15)}<sup>o</sup>C`;
+    nature.innerHTML=`${weather_data.weather[0].description}`;
+    speed.innerHTML=`${weather_data.wind.speed}Km/H`;
+    perc.innerHTML=`${weather_data.main.humidity}%`;
+
+    switch(weather_data.weather[0].description){
+        case 'Clouds':
+            weather_img.src="./assets/cloud.png"; 
+            break;
+        case 'Clear':
+            weather_img.src="./assets/clear.png";
+            break;
+        case 'Rain':
+            weather_img.src="./assets/rain.png";
+            break;
+        case 'Snow':
+            weather_img.src="./assets/snow.png";
+            break;
+        case 'Mist':
+            weather_img.src="./assets/mist.png";
+    }
+
+
+}
+searchBtn.addEventListener('click',()=>{
+    fetchWeather(searchBox.value);
+})
